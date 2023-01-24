c221d58d7bc962d8a5188528a850c3bd789f596b
mjobe2 <122488107+mjobe2@users.noreply.github.com>
2023-01-23 20:41:04 -0500

    Animation and Conditional Check in

---
+++ Animation and Conditional Operators Exercises/app.js
@@ -0,0 +1,10 @@
+//Do the world wrap option
+//
+//make canvas 800x600
+//have to set a radious for out ball
+// make variable for direction set it = 1
+//if x> width than multiply direction by -1
+//put background(0) after function draw() so that frames you only see recent circle
+//if x is less than radious of circle is less than 
+//add whatever (speed of 5 pixels x the direction) pixels to x to move it and in right direction
+//when the mouse passes midway point of the width, change the ball color using mouseX
