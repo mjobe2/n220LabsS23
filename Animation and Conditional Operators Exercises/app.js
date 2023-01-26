f395573195a1019b99554e83a47afe44f357924a
mjobe2 <122488107+mjobe2@users.noreply.github.com>
2023-01-25 21:09:54 -0500

    Correct colors

--- Animation and Conditional Operators Exercises/app.js
+++ Animation and Conditional Operators Exercises/app.js
@@ -1,13 +1,11 @@
-//Do the world wrap option
+//Do the puck side option
 //
-//make canvas 800x600
-//have to set a radious for out ball
-// make variable for direction set it = 1
-//if x> width than multiply direction by -1
-//put background(0) after function draw() so that frames you only see recent circle
-//if x is less than radious of circle is less than 
-//add whatever (speed of 5 pixels x the direction) pixels to x to move it and in right direction
-//when the mouse passes midway point of the width, change the ball color using mouseX
+//make canvas 400x300
+//put background() after function draw() so that frames you only see recent circle
+//use mouseX to draw ball where mouse is
+//if the mouse passes midway point of the width, change the ball color using mouseX
+//if mouse on left side, fill() blue
+//if mouse on right side fill() red
 let x=mouseX;
 
 function setup(){
