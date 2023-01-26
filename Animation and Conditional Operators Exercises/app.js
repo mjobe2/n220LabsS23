3b82d626501126f210e1e411936886203194659e
mjobe2 <122488107+mjobe2@users.noreply.github.com>
2023-01-25 21:03:17 -0500

    Code for Animation and Conditional Assignment

--- Animation and Conditional Operators Exercises/app.js
+++ Animation and Conditional Operators Exercises/app.js
@@ -8,3 +8,22 @@
 //if x is less than radious of circle is less than 
 //add whatever (speed of 5 pixels x the direction) pixels to x to move it and in right direction
 //when the mouse passes midway point of the width, change the ball color using mouseX
+let x=mouseX;
+
+function setup(){
+    createCanvas(400,300);
+    background(204);
+    fill(200,0,0);
+}
+
+function draw(){
+    background(204)
+    circle(mouseX,mouseY,20);
+    if(mouseX>200){
+        fill(200,0,0)
+        }
+        if (mouseX<200){
+            fill(0,0,200)
+        }
+    }
+    
