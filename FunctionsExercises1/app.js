22eec03fbefbd41eac2e17c57061a99d86648852
mjobe2 <122488107+mjobe2@users.noreply.github.com>
2023-01-30 16:14:45 -0500

    Algorithm Check In

---
+++ FunctionsExercises1/app.js
@@ -0,0 +1,4 @@
+//create a function for drawing a car
+//need variables for x and y values
+//create another function to have this car be drawn based upon mouseX and mouseY
+////Possibly going to have make an array to get more multiple compositions to pop up
