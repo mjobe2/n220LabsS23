a473b0481ca57d9653ca6d3ad929c807db0a69ae
mjobe2 <122488107+mjobe2@users.noreply.github.com>
2023-02-23 22:43:13 -0500

    had to make a copy because cant get github to open it

---
+++ LoopsAndArrays/RunnerUp2/RunnerUp2.js
@@ -0,0 +1,29 @@
+// 3 divs for each number
+//array of numbers need to use the []
+//order from smallest to largest
+
+
+let myWords= document.getElementById("number")
+let mySecond= document.getElementById("runnerup")
+let myThird= document.getElementById("thirdplace")
+
+
+myWords.style.backgroundColor="blue";
+myWords.style.height=100+"px";         //this section sets the properties of our square
+myWords.style.width=100+"px";
+
+
+let i=[5,7,12];
+ let t=0
+while (t<1){
+    t++;                 //only need to run this once
+myWords.innerHTML+= i[0];      //selects the element postion from the array
+mySecond.innerHTML+=  i[1];
+myThird.innerHTML+= i[2];
+
+}
+
+
+document.body.appendChild("number")
+document.body.appendChild("runnerup")
+document.body.appendChild("thirdplace")
